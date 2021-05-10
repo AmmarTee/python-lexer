eb2b44409cdef0bab23ea20572a17f3ff35e64bc
Ammar <57910338+AmmarTee@users.noreply.github.com>
2021-05-11 02:33:20 +0500

    Project Submission

---
+++ program.c
@@ -0,0 +1,19 @@
+int main()
+{
+    float j;
+    j = 1.4;
+    int i, inc, m;
+    i = 2;
+    inc = 8;
+
+    while (i < m)
+    {
+        i = i + inc;
+        j = j*i;
+    }
+
+    if (j > 15.0)
+    {
+        print(j + i);
+    }
+}
